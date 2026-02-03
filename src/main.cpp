bf130bbbe07d878c172a7983f6700ba0e17a0601
BhartiSingh005 <bharti.singh_cs.aiml24@gla.ac.in>
2026-02-03 09:19:13 +0530

    implement ultrasonic distance measurement in loop

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,17 @@ void setup() {
 }
 
 void loop() {
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    long duration = pulseIn(echoPin, HIGH);
+    long distance = duration * 0.034 / 2;
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println(" cm");
+    delay(1000);
 
 
 }
